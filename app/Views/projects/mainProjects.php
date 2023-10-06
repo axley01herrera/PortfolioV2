d82e19c88652b91b1f0fe9e392786044ecc60e9f
Axley <axley01herrera@gmail.com>
2023-10-06 20:10:19 +0100

    updates

--- app/Views/projects/mainProjects.php
+++ app/Views/projects/mainProjects.php
@@ -17,27 +17,13 @@
     <div class="card mt-5">
         <div class="row no-gutters align-items-center">
             <div class="col-md-4">
-                <img class="card-img img-fluid" src="<?php echo base_url('public/assets/images/projects/appointment01.jpg'); ?>" alt="tpv">
+                <img class="card-img img-fluid" src="<?php echo base_url('public/assets/images/projects/project01.jpg'); ?>" alt="tpv">
             </div>
             <div class="col-md-8">
                 <div class="card-body">
-                    <h5 class="card-title mb-1">Tu Negocio Citas Online</h5>
+                    <h5 class="card-title mb-1">Tu Negocio Online</h5>
                     <p class="card-text"><?php echo lang('Text.projects_tpv_appointment_exhibition'); ?></p>
                     <a href="https://negociocitas.online/" target="_blank" class="btn btn-primary"><?php echo lang('Text.projects_btn_view_demo'); ?></a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="card mt-5">
-        <div class="row no-gutters align-items-center">
-            <div class="col-md-4">
-                <img class="card-img img-fluid" src="<?php echo base_url('public/assets/images/projects/tpvshop01.jpg'); ?>" alt="tpv">
-            </div>
-            <div class="col-md-8">
-                <div class="card-body">
-                    <h5 class="card-title mb-1">SHOP TPV</h5>
-                    <p class="card-text"><?php echo lang('Text.projects_tpv_shop_exhibition'); ?></p>
-                    <a href="https://axleyherrera.com/public/projects/tpv_shop/Authentication" target="_blank" class="btn btn-primary"><?php echo lang('Text.projects_btn_view_demo'); ?></a>
                 </div>
             </div>
         </div>
