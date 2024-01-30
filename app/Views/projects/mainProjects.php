753ef97a04bee781375297cb823342660884627e
Axley <axley01herrera@gmail.com>
2024-01-30 09:12:40 +0000

    updates

--- app/Views/projects/mainProjects.php
+++ app/Views/projects/mainProjects.php
@@ -17,7 +17,7 @@
     <div class="card mt-5">
         <div class="row no-gutters align-items-center">
             <div class="col-md-4">
-                <img class="card-img img-fluid" src="<?php echo base_url('public/assets/images/projects/project01.jpg'); ?>" alt="tpv">
+                <img class="card-img img-fluid" src="<?php echo base_url('public/assets/images/projects/profileOnline.png'); ?>" alt="img">
             </div>
             <div class="col-md-8">
                 <div class="card-body">
@@ -25,75 +25,6 @@
                     <p class="card-text"><?php echo lang('Text.projects_tpv_appointment_exhibition'); ?></p>
                     <a href="https://negociocitas.online/" target="_blank" class="btn btn-primary"><?php echo lang('Text.projects_btn_view_demo'); ?></a>
                 </div>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="container">
-    <div class="row">
-        <div class="col-12 mt-5">
-            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-                <div class="carousel-inner" role="listbox">
-                    <div class="carousel-item active">
-                        <img class="d-block img-fluid mx-auto" src="<?php echo base_url('public/assets/images/projects/slider1p1.jpg'); ?>" alt="First slide" width="500px">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block img-fluid mx-auto" src="<?php echo base_url('public/assets/images/projects/slider2p1.jpg'); ?>" alt="Second slide" width="500px">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block img-fluid mx-auto" src="<?php echo base_url('public/assets/images/projects/slider3p1.jpg'); ?>" alt="Third slide" width="500px">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block img-fluid mx-auto" src="<?php echo base_url('public/assets/images/projects/slider4p1.jpg'); ?>" alt="Third slide" width="500px">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block img-fluid mx-auto" src="<?php echo base_url('public/assets/images/projects/slider5p1.jpg'); ?>" alt="Third slide" width="500px">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block img-fluid mx-auto" src="<?php echo base_url('public/assets/images/projects/slider6p1.jpg'); ?>" alt="Third slide" width="500px">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block img-fluid mx-auto" src="<?php echo base_url('public/assets/images/projects/slider7p1.jpg'); ?>" alt="Third slide" width="500px">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block img-fluid mx-auto" src="<?php echo base_url('public/assets/images/projects/slider8p1.jpg'); ?>" alt="Third slide" width="500px">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block img-fluid mx-auto" src="<?php echo base_url('public/assets/images/projects/slider9p1.jpg'); ?>" alt="Third slide" width="500px">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block img-fluid mx-auto" src="<?php echo base_url('public/assets/images/projects/slider10p1.jpg'); ?>" alt="Third slide" width="500px">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block img-fluid mx-auto" src="<?php echo base_url('public/assets/images/projects/slider11p1.jpg'); ?>" alt="Third slide" width="500px">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block img-fluid mx-auto" src="<?php echo base_url('public/assets/images/projects/slider12p1.jpg'); ?>" alt="Third slide" width="500px">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block img-fluid mx-auto" src="<?php echo base_url('public/assets/images/projects/slider13p1.jpg'); ?>" alt="Third slide" width="500px">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block img-fluid mx-auto" src="<?php echo base_url('public/assets/images/projects/slider14p1.jpg'); ?>" alt="Third slide" width="500px">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block img-fluid mx-auto" src="<?php echo base_url('public/assets/images/projects/slider15p1.jpg'); ?>" alt="Third slide" width="500px">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block img-fluid mx-auto" src="<?php echo base_url('public/assets/images/projects/slider16p1.jpg'); ?>" alt="Third slide" width="500px">
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block img-fluid mx-auto" src="<?php echo base_url('public/assets/images/projects/slider17p1.jpg'); ?>" alt="Third slide" width="500px">
-                    </div>
-                </div>
-                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
             </div>
         </div>
     </div>
