c30725d5b1af336590936868acaa58072e7127c7
Axley <axley01herrera@gmail.com>
2024-03-12 19:21:17 +0000

    updts

--- app/Views/projects/mainProjects.php
+++ app/Views/projects/mainProjects.php
@@ -17,7 +17,21 @@
     <div class="card mt-5">
         <div class="row no-gutters align-items-center">
             <div class="col-md-4">
-                <img class="card-img img-fluid" src="<?php echo base_url('public/assets/images/projects/profileOnline.png'); ?>" alt="img">
+                <img class="card-img img-fluid" src="<?php echo base_url('public/assets/images/projects/barberHi.png'); ?>" alt="barberHi" />
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title mb-1"><?php echo lang('Text.projects_barber_hi'); ?></h5>
+                    <p class="card-text"><?php echo lang('Text.projects_barber_hi_exp'); ?></p>
+                    <a href="https://demo.barberhi.es/" target="_blank" class="btn btn-primary"><?php echo lang('Text.projects_btn_view_demo'); ?></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="card mt-5">
+        <div class="row no-gutters align-items-center">
+            <div class="col-md-4">
+                <img class="card-img img-fluid" src="<?php echo base_url('public/assets/images/projects/profileOnline.png'); ?>" alt="profileOnline" />
             </div>
             <div class="col-md-8">
                 <div class="card-body">
